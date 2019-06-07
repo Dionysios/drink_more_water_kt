f86ae8a2dbd35475d71d4914901d0bd790c4df95
Dionysios Papathanopoulos <dpapathanopoulos@webst.gr>
2019-06-07 15:49:15 +0200

    Adding some code on MainFragment

--- app/src/main/java/com/dionpapas/drink_more_water/fragments/MainFragment.kt
+++ app/src/main/java/com/dionpapas/drink_more_water/fragments/MainFragment.kt
@@ -1,9 +1,11 @@
 package com.dionpapas.drink_more_water.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -14,17 +16,35 @@ import com.dionpapas.drink_more_water.viewmodels.MainFragmentViewModel
 class MainFragment : androidx.fragment.app.Fragment() {
 
     private lateinit var mainFragmentViewModel: MainFragmentViewModel
+    private lateinit var statusTextView: TextView
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        mainFragmentViewModel = ViewModelProviders.of(this)
-            .get(MainFragmentViewModel::class.java)
 
+        val view: View = inflater!!.inflate(R.layout.fragment_main, container, false)
+        mainFragmentViewModel = ViewModelProviders.of(this).get(MainFragmentViewModel::class.java)
+
+      //  statusTextView = view.findViewById(R.id.tv_water_count)
         mainFragmentViewModel.getLatestCounter().observe(this, Observer<WaterEntry> {
-            Toast.makeText(context, "Water entry not saved!", Toast.LENGTH_SHORT).show()
+            //Toast.makeText(context, "Water entry not saved!", Toast.LENGTH_SHORT).show()
         })
 
-        return inflater!!.inflate(R.layout.fragment_main, container, false)
+//        statusTextView.setOnClickListener {
+//            Log.d("Incoming", "Selected1")
+//        }
+        // Return the fragment view/layout
+        return view
     }
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {        // Get the custom view for this fragment layout
+        // Get the text view widget reference from custom layout
+        val tv = view.findViewById<TextView>(R.id.tv_water_count)
+        // Set a click listener for text view object
+        tv.setOnClickListener{
+            // Change the text color
+            tv.text = "2"
+            // Show click confirmation
+            Toast.makeText(view.context,"TextView clicked.",Toast.LENGTH_SHORT).show()
+        }
+        // Return the fragment view/layout
+    }
 }
+
